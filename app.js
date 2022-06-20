931bdbafc5396b2509ea3e5137fdec5e1a7cec52
Kamaji Indraksh <kamaji@nbos.vinisoft.com>
2022-06-20 18:02:16 +0530

    29th push

--- app.js
+++ app.js
@@ -25,6 +25,9 @@ app.use('/serviceRequest', serviceRequestRoutes);
 const statusCheckingRoutes = require('./routers/statusRoutes');
 app.use('/status', statusCheckingRoutes);
 
+const validationRoutes = require('./routers/validation');
+app.use('/validation', validationRoutes);
+
 app.use((req,res,next)=>{
     res.status(404).json({
         error:"bad request"
---
+++ routers/validation.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router3 = express.Router();
+
+
+router3.get('/:phoneNumber', async(req,res)=>{
+    try{  
+        const number = req.params.phoneNumber;
+        if(number.length == 10)
+        {
+            if(number.charAt(0) == 9 || number.charAt(0) == 8 || number.charAt(0) == 7 || number.charAt(0) == 6){
+                res.send(number)
+            }
+            else{
+                res.send('please check your number')
+            }
+        }
+        else{
+            res.send('please enter 10 digit phone number')
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+router3.get('/validateServiceRequest/:serviceNumber', async(req,res)=>{
+    try{  
+        const serviceNumber = req.params.serviceNumber;
+        if(serviceNumber.length == 6)
+        {
+            if(serviceNumber.slice(0,2) == 'SR'){
+                res.send(serviceNumber)
+            }
+            else{
+                res.send('please enter your service number starts with - SR')
+            }
+        }
+        else{
+            res.send('please check your service number')
+        }
+    }
+    catch(err){
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router3;

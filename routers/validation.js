1e77ec14a2efd5154eff6a615a3b30239ec86818
Kamaji Indraksh <kamaji@nbos.vinisoft.com>
2022-06-22 18:25:33 +0530

    40th push

--- routers/validation.js
+++ routers/validation.js
@@ -2,21 +2,26 @@ const express = require('express');
 const router3 = express.Router();
 const validate = require('../model/validation');
 
-router3.post('/', (req,res)=>{
+router3.post('/', async(req,res)=>{
     try{  
-        const validationNumber = new validate({
-            validator: req.body.phoneNumber
-        });
+        console.log('1')
         const number = req.body.phoneNumber;
-        if(number.length() == 10)
+        if(number.length == 10)
         {
+            console.log('2')
             if(number.charAt(0) == 9 || number.charAt(0) == 8 || number.charAt(0) == 7 || number.charAt(0) == 6){
-                res.send(validationNumber);
+                const validationNumber = new validate({
+                    validator: number
+                });
+                console.log('3')
+                res.json(validationNumber.validator);
             }else{
+                console.log('4')
                 res.send('inside')
             }
         }
         else{
+            console.log('5')
             res.send('outside')
         }
     }catch(err){
@@ -29,13 +34,13 @@ router3.post('/', (req,res)=>{
 router3.post('/validateServiceRequest/', async(req,res)=>{
     try{  
         const serviceNumber = req.body.serviceNumber;
-        if(serviceNumber.length() == 6)
+        if(serviceNumber.length == 6)
         {
             if(serviceNumber.slice(0,2) == 'SR'){
                 const validationNumber1 = new validate({
                     validator: serviceNumber
                 })
-                res.json(validationNumber1)
+                res.json(validationNumber1.validator)
             }
         }
     }
